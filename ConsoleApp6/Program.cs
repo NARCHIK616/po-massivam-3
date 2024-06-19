70f9336f15276017328f6950559fc2a4dc8ecfc5
narek <narek@DESKTOP-98JS365>
2024-06-19 18:20:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 3, 7, 2, 8, 4 };
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        double average = (double)sum / numbers.Length;
+
+        Console.WriteLine($"Сумма элементов массива: {sum}");
+        Console.WriteLine($"Среднее арифметическое элементов массива: {average}");
+    }
+}
